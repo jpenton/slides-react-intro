47a1dd1e07e7d2c8d29e9c1714b95f52cb56a20b
Jonathan Penton <jonathan@penton.site>
2019-10-21 02:02:18 -0500

    Fix prism theme resolution

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 import { condensed, prism } from 'mdx-deck/themes'
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import oneDark from 'react-syntax-highlighter/dist/esm/styles/prism/atom-dark'
 
 export default prism({
   ...condensed,
