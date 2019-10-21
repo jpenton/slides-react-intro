1f0506ee92cf8bbeaebc3cce573bb10ca7879dd3
Jonathan Penton <jonathan@penton.site>
2019-10-21 01:48:10 -0500

    Code theming

---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { condensed, prism } from 'mdx-deck/themes'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+export default prism({
+  ...condensed,
+  prism: {
+    style: oneDark
+  },
+})
